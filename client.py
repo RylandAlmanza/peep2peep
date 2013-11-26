ee4d87ec2d9f30e1065dbc0284695ff87ad543a5
Ryland Rene Taylor-Almanza <ryland@quixotix.com>
2013-11-25 16:03:56 -0800

    Sends keep alive packets so connections aren't lost so fast. Also functions as more of a peer to peer chat now.

--- client.py
+++ client.py
@@ -1,39 +1,117 @@
 import socket
+import threading
 
 # Connect to the server on port 16551. Obviously UDP_IP should be changed to the
 # server's ip.
 UDP_IP = "76.105.244.177"
 UDP_PORT = 16551
 
-# Create the socket object
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+class ChatClient():
+    def __init__(self, logging_enabled):
+        # If logging_enabled is true, the client will update you on what it's
+        # doing.
+        self.logging_enabled = logging_enabled
 
-# And send the initial packet so that the server can collect your ip and port
-sock.sendto("info packet", (UDP_IP, UDP_PORT))
+        # While this variable is true, all threads will continue running
+        self.running = True
 
-# Server should send you a packet with your peer's ip and port in a string with
-# the format "ip port" once it has both of the peers' ip's and ports
-(data, address) = sock.recvfrom(1024)
+        # Create the socket object
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Because the ip and port recieved are seperated by a space, split it in to two
-# parts at the space
-split_data = data.split(" ")
+        # And send the initial packet so that the server can collect your ip and
+        # port
+        self.log("Sending information packet to pairing server...")
+        self.sock.sendto("info packet", (UDP_IP, UDP_PORT))
+        self.log("Packet sent.")
 
-# Peer is a tuple, with the format (ip, port) which should now be stored in the
-# array split_data
-peer = (split_data[0], int(split_data[1]))
+        # Server should send you a packet with your peer's ip and port in a
+        # string with the format "ip port" once it has both of the peers' ip's
+        # and ports
+        data = "stayalive"
+        # Keep looping until we get something other than a keep-alive packet
+        while data == "stayalive":
+            self.log("Waiting for pairing server to send peer information...")
+            (data, address) = self.sock.recvfrom(1024)
+            if (data == "stayalive"):
+                self.log("Received keep-alive packet from server.")
+            else:
+                self.log("Peer information received.")
 
-# Send your peer the text "pickle", so they know it's you. Who else would send
-# the word pickle?
-sock.sendto("pickle", peer)
+        # Because the ip and port recieved are seperated by a space, split it
+        # into two parts at the space
+        split_data = data.split(" ")
 
-# Your peer should be sending you something too. I bet they'll never think to
-# send pickle
-(peers_message, _) = sock.recvfrom(1024)
+        # Peer is a tuple, with the format (ip, port) which should now be stored
+        # in the array split_data
+        self.peer = (split_data[0], int(split_data[1]))
+        self.log("Will be talking to " + \
+                 self.peer[0] + \
+                 " on port " + \
+                 str(self.peer[1]) + \
+                 ".")
 
-print("Your peer sent: " + peers_message)
+        # Create a new thread for sending keep alive packets to your peer
+        keep_alive_thread = threading.Thread()
+        # And set it's activity to our keep_alive_activity
+        keep_alive_thread.run = self.keep_alive_activity
 
-# If by some crazy coincidence, your peer also sent the word pickle
-if (peers_message == "pickle"):
-    # Vocalize your shock
-    print("WTF!?!")
+        # Create a new thread to receive messages on
+        receive_thread = threading.Thread()
+        # And set it's activity to our receive_activity
+        receive_thread.run = self.receive_activity
+
+        # Start receiving messages
+        receive_thread.start()
+        # And sending keep-alive packets
+        keep_alive_thread.start()
+
+        self.prompt_for_message()
+
+    # This activity sends regular packets to our peer to keep the connection
+    # alive
+    def keep_alive_activity(self):
+        # If the application is still supposed to be running
+        if (self.running):
+            # We send a packet to our peer
+            self.sock.sendto("stayalive", self.peer)
+            # And then schedule the next time we send another keep-alive packet
+            threading.Timer(10, self.keep_alive_activity).start()
+
+    # This activity will wait for messages from your peer
+    def receive_activity(self):
+        # While the application is still supposed to be running
+        while self.running:
+            # Get your peer's message
+            (peers_message, _) = self.sock.recvfrom(1024)
+            # If it's not just a keep-alive packet
+            if (peers_message != "stayalive"):
+                # Print your peer's message
+                print("\nYour peer sent: " + peers_message)
+
+    # This method starts prompting you for messages to send
+    def prompt_for_message(self):
+        # The message variable will store the message you want to send your peer
+        message = ""
+        # If the message doesn't say "quit", just keep prompting to send a
+        # message
+        while message != "quit":
+            # Get the message you want to send and store it in the message
+            # variable
+            message = raw_input("> ")
+            # Send your super personalized message to your peer
+            self.sock.sendto(message, self.peer)
+        
+        # If we got to this line, it means "quit" was entered. We should
+        # probably stop the program.
+        running = False
+        self.sock.close()
+
+    # Prints a message only if logging is enabled
+    def log(self, message):
+        # If logging is enabled, as determined by the logging_enabled member
+        if self.logging_enabled:
+            # Print the message
+            print(message)
+
+if __name__ == '__main__':
+    ChatClient(True)
--- server.py
+++ server.py
@@ -1,43 +1,103 @@
 # Server to test UDP hole punching
 
 import socket
+import threading
 
 # Run server on port 16551
 UDP_IP = "0.0.0.0"
 UDP_PORT = 16551
-
-# This array will store the ip and port that each client used to send the
-# initial packet
-clients = []
 
 # The first client in the array will be referred to as CLIENT_A
 CLIENT_A = 0
 # And the second as CLIENT_B
 CLIENT_B = 1
 
-# Create the socket object
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# And bind it to the port defined earlier
-sock.bind((UDP_IP, UDP_PORT))
+class ChatServer():
+    def __init__(self, logging_enabled):
+        # If logging_enabled is true, the server will update you on what it's
+        # doing.
+        self.logging_enabled = logging_enabled
 
-# This loop waits for 2 packets, and then stores the ip and port that the packet
-# was sent from. There should be one from CLIENT_A and one from CLIENT_B.
-for i in range(2):
-    # address is a tuple: (ip, port)
-    (_, address) = sock.recvfrom(1024)
-    # Append the ip and port tuple to the clients list
-    clients.append(address)
+        # Store whether or not we have successfully paired two clients in paired
+        self.paired = False
 
-# Store CLIENT_A's ip and port in the variables ip and port. In that order, of
-# course. Just want to clarify.
-(ip, port) = clients[CLIENT_A]
-# Send CLIENT_A's ip and port (concatenated into a string) to CLIENT_B
-sock.sendto(ip + " " + str(port), clients[CLIENT_B])
+        # This array will store the ip and port that each client used to send the
+        # initial packet
+        self.clients = []
 
-# Store CLIENT_B's ip and port in the variables ip and port.
-(ip, port) = clients[CLIENT_B]
-# Send CLIENT_B;s ip and port (concatenated into a string) to CLIENT_B
-sock.sendto(ip + " " + str(port), clients[CLIENT_A])
+        # Create the socket object
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # And bind it to the port defined earlier
+        self.sock.bind((UDP_IP, UDP_PORT))
+        self.log("Binded to " + UDP_IP + " on port " + str(UDP_PORT) + ".")
 
-# Well done server. Your service is no longer needed. Grab yourself a beer.
-print("Drinking a beer...")
+        # Create a new thread for sending keep alive packets to our clients,
+        keep_alive_thread = threading.Thread()
+        # set it's activity to our keep_alive_activity,
+        keep_alive_thread.run = self.keep_alive_activity
+        # And start it
+        keep_alive_thread.start()
+
+        # This loop waits for 2 packets, and then stores the ip and port that 
+        # the packet was sent from. There should be one from CLIENT_A and one
+        # from CLIENT_B.
+        self.log("Waiting for clients...")
+        for i in range(2):
+            # address is a tuple: (ip, port)
+            (_, address) = self.sock.recvfrom(1024)
+            # Append the ip and port tuple to the clients list
+            self.clients.append(address)
+            if i < 1:
+                self.log("Client obtained. Waiting for a second one...")
+            else:
+                self.log("Both clients obtained.")
+
+        # Store CLIENT_A's ip and port in the variables ip and port. In that
+        # order, of course. Just want to clarify.
+        (ip, port) = self.clients[CLIENT_A]
+        # Send CLIENT_A's ip and port (concatenated into a string) to CLIENT_B
+        self.log("Sending Client A's information to Client B...")
+        self.sock.sendto(ip + " " + str(port), self.clients[CLIENT_B])
+        self.log("Information sent.")
+
+        # Store CLIENT_B's ip and port in the variables ip and port.
+        (ip, port) = self.clients[CLIENT_B]
+        # Send CLIENT_B;s ip and port (concatenated into a string) to CLIENT_B
+        self.log("Sending Client B's information to Client A...")
+        self.sock.sendto(ip + " " + str(port), self.clients[CLIENT_A])
+        self.log("Information received.")
+
+        # Both clients should be paired now
+        self.paired = True
+        self.log("Both clients should now be paired.")
+
+        # Well done server. Your service is no longer needed. Grab yourself a
+        # beer.
+        self.log("Job done.")
+        self.log("Drinking a beer...")
+
+    # This activity sends regular packets to our clients to keep the connection
+    # alive
+    def keep_alive_activity(self):
+        # If we have at least one client
+        if (len(self.clients) > 0):
+            for i in range(len(self.clients)):
+                # We send a packet to our clients
+                self.sock.sendto("stayalive", self.clients[i])
+                self.log("keep alive packet sent to " + self.clients[i][0])
+                # And then schedule the next time we send another keep-alive
+                # packet
+        # If we still haven't paired the clients
+        if (self.paired == False):
+            # Make sure we keep sending keep-alive packets.
+            threading.Timer(10, self.keep_alive_activity).start()
+
+    # Prints a message only if logging is enabled
+    def log(self, message):
+        # If logging is enabled, as determined by the logging_enabled member
+        if self.logging_enabled:
+            # Print the message
+            print(message)          
+
+if __name__ == '__main__':
+    ChatServer(True)
